a33fe915479daf09082c338e23b6f0862a69f770
RQISL <creativewb50@gmail.com>
2023-07-21 21:00:51 +0100

    Update urls.py

--- order_foods/urls.py
+++ order_foods/urls.py
@@ -1,7 +1,9 @@
 from . import views
 from django.urls import path
+from order_foods.views import Overlay
 
 urlpatterns = [
     path("", views.PostList.as_view(), name="home"),
+    path("overlay/", Overlay.as_view(), name="overlay"),
     path('<slug:slug>/', views.PostDetail.as_view(), name='contact_and_feedback'),
 ]
