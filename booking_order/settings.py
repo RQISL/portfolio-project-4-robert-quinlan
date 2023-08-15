96638212ba2e3892b49bc2774aaf0f71f953f3aa
Robert Quinlan <creativewb50@gmail.com>
2023-08-15 12:53:39 +0000

    set False again

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -38,10 +38,10 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-rqisl-portfolio-project-2n9gn2h1od.us2.codeanyapp.com']
+    'https://8000-rqisl-portfolio-project-2n9gn2h1od.us2.codeanyapp.com',  'order-foods-online-3e149702663b.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-d4cbkjh23oi.ws-eu103.gitpod.io',
                'order-foods-online-3e149702663b.herokuapp.com'
