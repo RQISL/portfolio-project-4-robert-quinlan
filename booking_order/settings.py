2f934e8f43c78ee47eb28343f9d274c75fd6ae8a
Robert Quinlan <creativewb50@gmail.com>
2023-08-15 10:10:31 +0000

    move to Gitpod and changed set to False for Heroku deployed

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 import os
 from pathlib import Path
-import dj_database_url
+# import dj_database_url
 import cloudinary
 
 from django.contrib.messages import constants as messages
@@ -41,7 +41,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-rqisl-portfolio-project-2n9gn2h1od.us2.codeanyapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolio-project-2n9gn2h1od.us2.codeanyapp.com',
                  'order-booking-online-f33d634e6883.herokuapp.com',
