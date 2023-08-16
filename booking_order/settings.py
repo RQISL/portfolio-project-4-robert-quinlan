de46624f7b1b7a3338ea6d5d4fa8112441eb2fd2
Robert Quinlan <creativewb50@gmail.com>
2023-08-16 13:59:38 +0000

    modified responsive on Home, Login, Nav-bar and edited images

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -41,9 +41,9 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-rqisl-portfolio-project-2n9gn2h1od.us2.codeanyapp.com',  'order-foods-online-3e149702663b.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-d4cbkjh23oi.ws-eu103.gitpod.io',
+ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-haoet8dajn1.ws-eu103.gitpod.io',
                'order-foods-online-3e149702663b.herokuapp.com',
                  'localhost']
 
