44e39043cb25502b31e8b6a430234501d9abcda7
Robert Quinlan <creativewb50@gmail.com>
2023-08-20 15:03:57 +0000

    set to True

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -41,7 +41,7 @@ CSRF_TRUSTED_ORIGINS = [
     'https://8000-rqisl-portfolioproject-mdwlz4aq5x7.ws-eu104.gitpod.io',  'order-foods-online-3e149702663b.herokuapp.com']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-mdwlz4aq5x7.ws-eu104.gitpod.io',
                'order-foods-online-3e149702663b.herokuapp.com',
