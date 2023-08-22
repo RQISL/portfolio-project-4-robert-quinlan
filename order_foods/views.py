43038f2d6548fbeaa1f636f9bd724fce28c1b8a9
Robert Quinlan <creativewb50@gmail.com>
2023-08-22 21:21:28 +0000

    fix error on view.py

--- order_foods/views.py
+++ order_foods/views.py
@@ -101,13 +101,13 @@ class Order(View):
 
             order_items['items'].append(item_data)
 
-            price = 0
-            item_ids = []
+        price = 0
+        item_ids = []
 
         for item in order_items['items']:
             price += item['price']
             item_ids.append(item['id'])
-
+        
         order = OrderModel.objects.create(
             price=price,
             name=name,
