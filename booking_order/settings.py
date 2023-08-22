5867d1f1efc0c8a70b9280246d293c80d76d8e4d
Robert Quinlan <creativewb50@gmail.com>
2023-08-22 15:55:28 +0000

    add new url heroku in host

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -38,13 +38,13 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY')
 CSRF_TRUSTED_ORIGINS = [
-    'https://8000-rqisl-portfolioproject-mdwlz4aq5x7.ws-eu104.gitpod.io',  'order-foods-online-65f34d203da9.herokuapp.com']
+    'https://8000-rqisl-portfolioproject-mdwlz4aq5x7.ws-eu104.gitpod.io',  'https://project-4-order-foods-65605ab1b9f2.herokuapp.com/']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = 
 
 ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-mdwlz4aq5x7.ws-eu104.gitpod.io',
-               'order-foods-online-65f34d203da9.herokuapp.com',
+               'project-4-order-foods-65605ab1b9f2.herokuapp.com',
                  'localhost']
 
 
