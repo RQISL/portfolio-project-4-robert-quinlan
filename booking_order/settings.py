1e14079cc0fd3eec00703841c98459c3725c2d9a
Robert Quinlan <creativewb50@gmail.com>
2024-02-01 19:41:49 +0000

    fix host

--- booking_order/settings.py
+++ booking_order/settings.py
@@ -38,11 +38,7 @@ CSRF_TRUSTED_ORIGINS = [
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [
-                '8000-rqisl-portfolioproject-onj2fh4s4gp.ws-eu107.gitpod.io',
-                'project-4-order-foods-65605ab1b9f2.herokuapp.com',
-                'localhost'
-                ]
+ALLOWED_HOSTS = ['8000-rqisl-portfolioproject-onj2fh4s4gp.ws-eu107.gitpod.io','project-4-order-foods-65605ab1b9f2.herokuapp.com', 'localhost']
 
 
 # Application definition
