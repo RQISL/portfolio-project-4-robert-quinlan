db817f3b8265a37591ba4c25e3aeb8fdc3fcaf43
Robert Quinlan <creativewb50@gmail.com>
2024-02-01 19:00:22 +0000

    Replace from ProfileView to Profile

--- order_foods/admin.py
+++ order_foods/admin.py
@@ -1,8 +1,7 @@
-from .models import ProfileView
 from django.contrib import admin
-from .models import MenuItem, Category, OrderModel, ProfileView, ContactView
+from .models import MenuItem, Category, OrderModel, Profile, ContactView
 admin.site.register(MenuItem)
 admin.site.register(Category)
 admin.site.register(OrderModel)
-admin.site.register(ProfileView)
+admin.site.register(Profile)
 admin.site.register(ContactView)
